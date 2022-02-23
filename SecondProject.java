b3213ccbdb2fc142b06a83f1d7d3c92eb925fa38
ShyamNkumar <99385805+ShyamNkumar@users.noreply.github.com>
2022-02-23 10:44:26 +0530

    Update SecondProject.java

--- SecondProject.java
+++ SecondProject.java
@@ -4,6 +4,8 @@ public static void main(String[] args)
 {
 System.out.println("Shyam");
 System.out.println("Sam");
-System.out.println("Sham")
+System.out.println("Sham");
+System.out.println("Chennai");
+System.out.println("Coimbatore");
 }
 }
